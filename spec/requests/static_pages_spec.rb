7717f546e00b6f0d4e8488e3c0e829f2f15e4449
Sungho Park <caseeker@gmail.com>
2013-11-17 19:27:06 -0600

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -49,7 +49,7 @@ describe "Static pages" do
     click_link "Sign up now!"
     expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
-    expect(page).to have_title(full_title('Sign up'))# fill in
+    expect(page).to have_title(full_title(''))# fill in
   end
 end
 
